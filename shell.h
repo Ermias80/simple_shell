48c392dd638bb2d6fc1be8ae4ade34b21d285dd2
Ermias80 <ermiasmaz8069@gmail.com>
2024-02-19 23:59:24 -0800

    all

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+//#define PRINT(c) (write(STDERR_FILENO, c, _strlen(c)))
+
+/**Standard Library*/
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+char *read_line(void);
+void prompt(void);
+void PRINT(const char *message);
+int strlen(char *s);
+
+#endif
